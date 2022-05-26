7cb7bb95d776134cf46e9ab149b2370b76bebbd6
Yaroslav Spirin <yspirin@plesk.com>
2022-05-26 18:40:29 +0300

    Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\ProcessPodcast;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')->hourly();
+        $schedule->job(new ProcessPodcast())
+                 ->weekdays()
+                 ->hourly()
+                 ->timezone('America/Chicago')
+                 ->between('8:00', '17:00');
+        $schedule->exec('node /home/forge/script.js');
     }
 
     /**
