f864b7a02e85b7787600ae277d8a8f3f0bf4da3a
Yaroslav Spirin <yspirin@plesk.com>
2022-06-07 17:03:06 +0300

    Schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,13 +16,20 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')->hourly();
+        $schedule->command('inspire')
+                 ->hourly()
+                ->everyTenMinutes()
+                ->days(Schedule::TUESDAY);
         $schedule->job(new ProcessPodcast())
                  ->weekdays()
                  ->hourly()
-                 ->timezone('America/Chicago')
-                 ->between('8:00', '17:00');
-        $schedule->exec('node /home/forge/script.js');
+                 ->days(Schedule::SATURDAY)
+                 ->twiceMonthly(13, 15, '22:32')
+                 ->everyThreeHours();
+        $schedule->job(new ProcessPodcast())->cron('15 10,13,14,16,22,23 * * 1,4');
+        $date = date("H:i:s");
+        $schedule->exec("echo '' >> cron/schedule-$date.txt")
+                 ->description('Create file');
     }
 
     /**
